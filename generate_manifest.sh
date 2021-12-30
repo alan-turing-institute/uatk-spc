f2cc16d3407e1b498fa8ccee2f1086a10fead246
Dustin Carlino <dabreegster@gmail.com>
2021-12-30 10:17:52 +0000

    Start keeping a manifest of all data, so I can make sure code changes do or don't modify output as expected

---
+++ generate_manifest.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Generates a manifest file of everything in raw_data. This is a quick
+# workaround before figuring out easier file management, either with Git-LFS,
+# https://github.com/a-b-street/abstreet/tree/master/updater, or something else
+
+set -e
+
+manifest=manifest.csv
+echo 'file,bytes,checksum' > $manifest
+
+for file in `find raw_data/ -type f`; do
+	echo $file
+	checksum=`md5sum $file | cut -d ' ' -f1`
+	bytes=`stat -c %s $file`
+	echo "$file,$bytes,$checksum" >> $manifest
+done
