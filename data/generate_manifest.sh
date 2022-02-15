9670ae2e276560b5daa0cbecf9320065dc07f068
Dustin Carlino <dabreegster@gmail.com>
2022-02-15 09:12:29 +0000

    Move manifest files to data/ to keep the root dir focused

--- data/generate_manifest.sh
+++ data/generate_manifest.sh
@@ -2,10 +2,12 @@
 # Generates a manifest file of everything in raw_data. This is a quick
 # workaround before figuring out easier file management, either with Git-LFS,
 # https://github.com/a-b-street/abstreet/tree/master/updater, or something else
+#
+# Run from the repository's root directory
 
 set -e
 
-manifest=manifest.csv
+manifest=data/manifest.csv
 echo 'file,bytes,checksum' > $manifest
 
 for file in `find data/raw_data/ -type f`; do
