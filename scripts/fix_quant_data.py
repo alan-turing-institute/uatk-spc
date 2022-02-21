6dd133237f6e02f31c776e18018ac594de2c2c8a
Dustin Carlino <dabreegster@gmail.com>
2022-02-21 12:27:29 +0000

    Fix directory structure in one of the Python interop scripts

--- scripts/fix_quant_data.py
+++ scripts/fix_quant_data.py
@@ -7,5 +7,5 @@ import pickle
 
 for name in ['hospitalProbHij', 'primaryProbPij', 'retailpointsProbSij', 'secondaryProbPij']:
     print(f'Converting {name} from pickle to numpy format')
-    data = pickle.load(open(f'raw_data/nationaldata/QUANT_RAMP/{name}.bin', 'rb'))
-    numpy.save(f'raw_data/nationaldata/QUANT_RAMP/{name}.npy', data)
+    data = pickle.load(open(f'data/raw_data/nationaldata/QUANT_RAMP/{name}.bin', 'rb'))
+    numpy.save(f'data/raw_data/nationaldata/QUANT_RAMP/{name}.npy', data)
