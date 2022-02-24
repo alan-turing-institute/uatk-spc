9e55c730c2150232d50ea3fe43f1cf470bbca22f
Dustin Carlino <dabreegster@gmail.com>
2022-02-24 12:30:58 +0000

    Split commuting assignment and business scraping. #6

--- src/init/commuting.rs
+++ src/init/commuting.rs
@@ -13,44 +13,7 @@ use crate::utilities::{print_count, progress_count};
 use crate::{Activity, Population, Venue, VenueID, MSOA};
 
 pub fn create_commuting_flows(population: &mut Population, rng: &mut StdRng) -> Result<()> {
-    // Only keep businesses in MSOAs where somebody lives.
-    //
-    // The rationale: if we're restricting the study area, we don't want to send people to work far
-    // away, where the only activity occuring is work.
-    //
-    // TODO Only people who work -- does that matter?
-    let msoas = population.unique_msoas();
-
-    // Find all of the businesses, grouped by the Standard Industry Classification.
-    info!("Finding all businesses");
-    let mut businesses_per_sic: HashMap<usize, Vec<BusinessID>> = HashMap::new();
-    let mut business_locations: HashMap<BusinessID, Point<f32>> = HashMap::new();
-    let mut available_jobs_per_business: HashMap<BusinessID, usize> = HashMap::new();
-    let mut total_jobs = 0;
-    for rec in csv::Reader::from_reader(File::open(
-        "data/raw_data/nationaldata/businessRegistry.csv",
-    )?)
-    .deserialize()
-    {
-        let rec: Row = rec?;
-        if msoas.contains(&rec.msoa) {
-            // The CSV has string IDs, but they're not used anywhere else. Use integer IDs,
-            // which're much faster to copy around.
-            let id = BusinessID(business_locations.len().try_into()?);
-            businesses_per_sic
-                .entry(rec.sic1d07)
-                .or_insert_with(Vec::new)
-                .push(id);
-            business_locations.insert(id, Point::new(rec.lng, rec.lat));
-            available_jobs_per_business.insert(id, rec.size);
-            total_jobs += rec.size;
-        }
-    }
-    info!(
-        "{} jobs available among {} businesses",
-        print_count(total_jobs),
-        print_count(business_locations.len())
-    );
+    let mut businesses = load_businesses(population)?;
 
     // Assign numeric VenueIDs as we decide to use a business.
     let mut business_to_venue: HashMap<BusinessID, VenueID> = HashMap::new();
@@ -69,11 +32,13 @@ pub fn create_commuting_flows(population: &mut Population, rng: &mut StdRng) -> 
             // number of available jobs there and the inverse square distance between the person
             // and business.
             let mut choices: Vec<(BusinessID, f32)> = Vec::new();
-            if let Some(list) = businesses_per_sic.get(&sic) {
+            if let Some(list) = businesses.list_per_sic.get(&sic) {
                 for id in list {
-                    let jobs_available = available_jobs_per_business[id];
+                    let jobs_available = businesses.available_jobs[id];
                     if jobs_available > 0 {
-                        let dist = person.location.haversine_distance(&business_locations[id]);
+                        let dist = person
+                            .location
+                            .haversine_distance(&businesses.locations[id]);
                         choices.push((*id, (jobs_available as f32) / dist.powi(2)));
                     }
                 }
@@ -86,7 +51,7 @@ pub fn create_commuting_flows(population: &mut Population, rng: &mut StdRng) -> 
                     None => {
                         let venue_id = VenueID(business_to_venue.len());
                         business_to_venue.insert(business_id.clone(), venue_id);
-                        let location = &business_locations[&business_id];
+                        let location = &businesses.locations[&business_id];
                         venues.push(Venue {
                             id: venue_id,
                             activity: Activity::Work,
@@ -101,8 +66,9 @@ pub fn create_commuting_flows(population: &mut Population, rng: &mut StdRng) -> 
                 person.flows_per_activity[Activity::Work] = vec![(venue_id, 1.0)];
 
                 // This job is gone
-                available_jobs_per_business
-                    .insert(*business_id, available_jobs_per_business[business_id] - 1);
+                businesses
+                    .available_jobs
+                    .insert(*business_id, businesses.available_jobs[business_id] - 1);
             } else {
                 // There were no remaining jobs for this SIC.
                 // TODO What should we do?
@@ -130,3 +96,55 @@ struct Row {
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
 struct BusinessID(u32);
+
+struct Businesses {
+    list_per_sic: HashMap<usize, Vec<BusinessID>>,
+    locations: HashMap<BusinessID, Point<f32>>,
+    available_jobs: HashMap<BusinessID, usize>,
+}
+
+fn load_businesses(population: &Population) -> Result<Businesses> {
+    let mut result = Businesses {
+        list_per_sic: HashMap::new(),
+        locations: HashMap::new(),
+        available_jobs: HashMap::new(),
+    };
+
+    // Only keep businesses in MSOAs where somebody lives.
+    //
+    // The rationale: if we're restricting the study area, we don't want to send people to work far
+    // away, where the only activity occuring is work.
+    //
+    // TODO Only people who work -- does that matter?
+    let msoas = population.unique_msoas();
+
+    // Find all of the businesses, grouped by the Standard Industry Classification.
+    info!("Finding all businesses");
+    let mut total_jobs = 0;
+    for rec in csv::Reader::from_reader(File::open(
+        "data/raw_data/nationaldata/businessRegistry.csv",
+    )?)
+    .deserialize()
+    {
+        let rec: Row = rec?;
+        if msoas.contains(&rec.msoa) {
+            // The CSV has string IDs, but they're not used anywhere else. Use integer IDs,
+            // which're much faster to copy around.
+            let id = BusinessID(result.locations.len().try_into()?);
+            result
+                .list_per_sic
+                .entry(rec.sic1d07)
+                .or_insert_with(Vec::new)
+                .push(id);
+            result.locations.insert(id, Point::new(rec.lng, rec.lat));
+            result.available_jobs.insert(id, rec.size);
+            total_jobs += rec.size;
+        }
+    }
+    info!(
+        "{} jobs available among {} businesses",
+        print_count(total_jobs),
+        print_count(result.locations.len())
+    );
+    Ok(result)
+}
