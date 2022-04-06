d6b51ed1eefe633ab93daadfdd8f842b23c77cd0
Dustin Carlino <dabreegster@gmail.com>
2022-04-06 18:04:51 +0100

    Remove copy of Params; I forgot we had it in this repo already

--- model/convert_snapshot.py
+++ model/convert_snapshot.py
@@ -2,6 +2,7 @@
 
 import click
 import numpy as np
+from aspics.params import Params
 import random
 import synthpop_pb2
 from collections import namedtuple
@@ -188,135 +189,6 @@ def get_place_coordinates(pop, id_mapping):
         result[place * 2 + 1] = location.longitude
 
     return result
-
-
-LocationHazardMultipliers = namedtuple(
-    "LocationHazardMultipliers",
-    ["retail", "nightclubs", "primary_school", "secondary_school", "home", "work"],
-)
-
-IndividualHazardMultipliers = namedtuple(
-    "IndividualHazardMultipliers", ["presymptomatic", "asymptomatic", "symptomatic"]
-)
-
-
-class Params:
-    def __init__(
-        self,
-        location_hazard_multipliers=LocationHazardMultipliers(
-            retail=0.0165,
-            nightclubs=0.0165,
-            primary_school=0.0165,
-            secondary_school=0.0165,
-            home=0.0165,
-            work=0.0,
-        ),
-        individual_hazard_multipliers=IndividualHazardMultipliers(
-            presymptomatic=1.0, asymptomatic=0.75, symptomatic=1.0
-        ),
-        obesity_multipliers=[1, 1, 1, 1],
-        cvd_multiplier=1,
-        diabetes_multiplier=1,
-        bloodpressure_multiplier=1,
-        overweight_sympt_mplier=1.46,
-    ):
-        if obesity_multipliers is None:
-            obesity_multipliers = [1, 1, 1, 1]
-        self.symptomatic_multiplier = 0.5
-        self.exposed_scale = 2.82
-        self.exposed_shape = 3.99
-        self.presymptomatic_scale = 2.45
-        self.presymptomatic_shape = 7.79
-        self.infection_log_scale = 0.35
-        self.infection_mode = 7.0
-        self.lockdown_multiplier = 1.0
-        self.place_hazard_multipliers = np.array(
-            [
-                location_hazard_multipliers.retail,
-                location_hazard_multipliers.nightclubs,
-                location_hazard_multipliers.primary_school,
-                location_hazard_multipliers.secondary_school,
-                location_hazard_multipliers.home,
-                location_hazard_multipliers.work,
-            ],
-            dtype=np.float32,
-        )
-
-        self.individual_hazard_multipliers = np.array(
-            [
-                individual_hazard_multipliers.presymptomatic,
-                individual_hazard_multipliers.asymptomatic,
-                individual_hazard_multipliers.symptomatic,
-            ],
-            dtype=np.float32,
-        )
-
-        self.mortality_probs = np.array(
-            [
-                0.00,
-                0.0001,
-                0.0001,
-                0.0002,
-                0.0003,
-                0.0004,
-                0.0006,
-                0.0010,
-                0.0016,
-                0.0024,
-                0.0038,
-                0.0060,
-                0.0094,
-                0.0147,
-                0.0231,
-                0.0361,
-                0.0566,
-                0.0886,
-                0.1737,
-            ],
-            dtype=np.float32,
-        )
-        self.obesity_multipliers = np.array(obesity_multipliers, dtype=np.float32)
-        self.symptomatic_probs = np.array(
-            [0.21, 0.21, 0.45, 0.45, 0.45, 0.45, 0.45, 0.69, 0.69], dtype=np.float32
-        )
-        self.cvd_multiplier = cvd_multiplier
-        self.diabetes_multiplier = diabetes_multiplier
-        self.bloodpressure_multiplier = bloodpressure_multiplier
-        self.overweight_sympt_mplier = overweight_sympt_mplier
-
-    def asarray(self):
-        """Pack the parameters into a flat array for uploading."""
-        return np.concatenate(
-            [
-                np.array(
-                    [
-                        self.symptomatic_multiplier,
-                        self.exposed_scale,
-                        self.exposed_shape,
-                        self.presymptomatic_scale,
-                        self.presymptomatic_shape,
-                        self.infection_log_scale,
-                        self.infection_mode,
-                        self.lockdown_multiplier,
-                    ],
-                    dtype=np.float32,
-                ),
-                self.place_hazard_multipliers,
-                self.individual_hazard_multipliers,
-                self.mortality_probs,
-                self.obesity_multipliers,
-                self.symptomatic_probs,
-                np.array(
-                    [
-                        self.cvd_multiplier,
-                        self.diabetes_multiplier,
-                        self.bloodpressure_multiplier,
-                        self.overweight_sympt_mplier,
-                    ],
-                    dtype=np.float32,
-                ),
-            ]
-        )
 
 
 if __name__ == "__main__":
