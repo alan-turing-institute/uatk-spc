745fee1b0af476b39720ccc015d0da614be08743
Dustin Carlino <dabreegster@gmail.com>
2022-04-19 11:11:53 +0100

    Making the new areas script more easy to use

--- scripts/select_msoas.py
+++ scripts/select_msoas.py
@@ -1,12 +1,43 @@
-# This script helps you define new study areas. You can write the output to a
-# new file, config/some_region.csv .
+# This script helps you define new study areas.
+#
+# You must run this in a Python environment with
+# https://pypi.org/project/click/ installed.
 
 import csv
+import click
+import os
 
-print('"MSOA11CD"')
-# You must run the SPC pipeline to download this file
-with open('data/raw_data/referencedata/lookUp.csv') as f:
-    for row in csv.DictReader(f):
-        # Modify these to select MSOAs based on different geographies
-        if row['LAD20NM'] == 'Liverpool':
-            print('"{}"'.format(row['MSOA11CD']))
+
+@click.command()
+@click.option(
+    "--lad",
+    default="Liverpool",
+    prompt="Name of Local Authority District",
+    help="Enter the name of the Local Authority District of your preference, to get the MSOA codes for that area",
+)
+def area(lad):
+    # By convention, use snake case (west_yorkshire_small)
+    region_name = lad.lower().replace(" ", "_")
+    filename = f"config/{region_name}.csv"
+    nonempty = False
+
+    with open(filename, "w") as output:
+        output.write('"MSOA11CD"\n')
+        # You must run the SPC pipeline to download this file
+        with open("data/raw_data/referencedata/lookUp.csv") as lookup:
+            for row in csv.DictReader(lookup):
+                if row["LAD20NM"] == lad:
+                    output.write('"{}"\n'.format(row["MSOA11CD"]))
+                    nonempty = True
+
+    if nonempty:
+        print(f"Wrote {filename}")
+    else:
+        os.remove(filename)
+        print(
+            f"No matches for LAD20NM = {lad} in data/raw_data/referencedata/lookUp.csv"
+        )
+
+
+if __name__ == "__main__":
+    area()
