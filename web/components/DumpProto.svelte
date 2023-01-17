308f43d98403b3e2426a1deb0c5d9adf96d9192a
Dustin Carlino <dabreegster@gmail.com>
2023-01-17 16:33:30 +0000

    Hover on MSOAs

--- web/components/DumpProto.svelte
+++ web/components/DumpProto.svelte
@@ -1,7 +1,9 @@
 <script>
   export let pop;
+  export let hoveredMsoa;
 </script>
 
+<h2>Selected? {hoveredMsoa}</h2>
 <p>{pop.households.length} households</p>
 <p>{pop.people.length} people</p>
 <p>{pop.people[0].toJsonString()} people</p>
