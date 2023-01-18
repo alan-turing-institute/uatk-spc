c9af26ff8ff1f82fe075e9ef86fdcacfe0a08d27
Dustin Carlino <dabreegster@gmail.com>
2023-01-18 10:16:28 +0000

    Add a few more categories to the choropleth

--- web/data.js
+++ web/data.js
@@ -2,13 +2,26 @@
 export function msoaStats(pop) {
   let households_per_msoa = {};
   let people_per_msoa = {};
+  let avg_age = {};
   for (let id of Object.keys(pop.infoPerMsoa)) {
     households_per_msoa[id] = 0;
     people_per_msoa[id] = 0;
+    avg_age[id] = 0.0;
   }
   for (let hh of pop.households) {
     households_per_msoa[hh.msoa11cd]++;
     people_per_msoa[hh.msoa11cd] += hh.members.length;
+    // Sum age per MSOA
+    for (let id of hh.members) {
+      avg_age[hh.msoa11cd] += pop.people[id].demographics.ageYears;
+    }
+  }
+
+  let avg_household_size = {};
+  for (let id of Object.keys(pop.infoPerMsoa)) {
+    // TODO Cast?
+    avg_household_size[id] = people_per_msoa[id] / households_per_msoa[id];
+    avg_age[id] /= people_per_msoa[id];
   }
 
   let msoas = {};
@@ -19,6 +32,8 @@ export function msoaStats(pop) {
         id,
         households: households_per_msoa[id],
         people: people_per_msoa[id],
+        avg_age: avg_age[id],
+        avg_household_size: avg_household_size[id],
       },
       geometry: {
         coordinates: [info.shape.map((pt) => [pt.longitude, pt.latitude])],
