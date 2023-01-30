1ca851b6e420bfab9cd928b6e3d62c7639be004e
Dustin Carlino <dabreegster@gmail.com>
2023-01-30 12:40:32 +0000

    Swap out plotly graph immediately

--- web/components/MsoaDetails.svelte
+++ web/components/MsoaDetails.svelte
@@ -4,8 +4,16 @@
   export let pop;
   export let clickedMsoa;
 
+  // Note this is always present in the template. If we hide it under the
+  // clickedMsoa condition, it doesn't get created before the reactive block
+  // below runs.
+  let div;
+
   let ages = [];
   $: {
+    if (div) {
+      Plotly.purge(div);
+    }
     ages = [];
     if (clickedMsoa) {
       for (let hh of pop.households) {
@@ -15,18 +23,17 @@
           }
         }
       }
-    }
-  }
 
-  // TODO This doesn't use latest ages, you have to click on and off an MSOA.
-  function plotlyAction(div) {
-    Plotly.newPlot(div, [{ x: ages, type: "histogram" }]);
+      Plotly.newPlot(div, [{ x: ages, type: "histogram" }], {
+        title: `Ages in ${clickedMsoa}`,
+      });
+    }
   }
 </script>
 
 {#if clickedMsoa}
   <h2>{clickedMsoa}</h2>
-  <div use:plotlyAction />
 {:else}
   <p>Click an MSOA for more details</p>
 {/if}
+<div bind:this={div} />
