e885945f8c5f7a7e8f52f1a44ec037ba136b934f
Hadrien Salat <hsalat@turing.ac.uk>
2023-01-31 18:44:05 +0000

    Update createMobilityFile.R
    New file with Google Mobility Reports up to 15/10/2022 for all UK uploaded to Azure inside `nationaldata-v2`. Changes to the R script to generate it. Note the change to the name of the area column from `CTY20` to `GoogleCTY_CNC` as County only partially reflects the names of the sub-regions at UK level.

--- scripts/data_prep/createMobilityFile.R
+++ scripts/data_prep/createMobilityFile.R
@@ -4,35 +4,32 @@ library(readr)
 library(tidyr)
 library(reshape2)
 
-### REQUIRED: Look up table (from Azure) in same folder
 ### Assumption: if percent value for a County is NA -> change value to national average
-  
+
 # Download latest file from google mobility
 options(timeout=600)
 download.file("https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv", 
-                destfile = "Google_Global_Mobility_Report.csv")
-  
+              destfile = "Google_Global_Mobility_Report.csv")
+
 gm <- read_csv("Google_Global_Mobility_Report.csv") %>% 
-    filter(country_region == "United Kingdom" & !is.na(sub_region_1))
+  filter(country_region == "United Kingdom" & !is.na(sub_region_1))
 gm <- gm %>% dplyr::select( c(sub_region_1,date,residential_percent_change_from_baseline))
-colnames(gm) <- c("CTY20","date","change")
+colnames(gm) <- c("GoogleCTY_CNC","date","change")
 gm$day <- as.numeric(gm$date)-min(as.numeric(gm$date))
 
-# Filter according to look up table and aggregate CTY/date
-ref <- read.csv("lookUp.csv")
-gm <- gm %>% filter(CTY20 %in% ref$GoogleMob)
+# Aggregate CTY/date
 gm <- gm[,c(1,2,4,3)]
-gm <- aggregate(gm$change, by = list(gm$CTY20,gm$date,gm$day),FUN = mean)
-colnames(gm) <- c("CTY20","date","day","change")
+gm <- aggregate(gm$change, by = list(gm$GoogleCTY_CNC,gm$date,gm$day),FUN = mean)
+colnames(gm) <- c("GoogleCTY_CNC","date","day","change")
 
-M <- matrix(nrow = length(unique(gm$CTY20)),ncol = length(unique(gm$day)),NA)
-rownames(M) <- unique(gm$CTY20)
+M <- matrix(nrow = length(unique(gm$GoogleCTY_CNC)),ncol = length(unique(gm$day)),NA)
+rownames(M) <- unique(gm$GoogleCTY_CNC)
 colnames(M) <- unique(gm$day)
 base <- melt(M)
 
-gm <- merge(base, gm, by.x = c("Var1","Var2"), by.y = c("CTY20","day"), all=T)
+gm <- merge(base, gm, by.x = c("Var1","Var2"), by.y = c("GoogleCTY_CNC","day"), all=T)
 gm <- gm[,c(1,4,2,5)]
-colnames(gm) <- c("CTY20","date","day","change")
+colnames(gm) <- c("GoogleCTY_CNC","date","day","change")
 
 # Change NA values to national average
 nat <- aggregate(gm$change, by = list(gm$day), FUN = mean, na.rm = TRUE)$x
@@ -45,3 +42,7 @@ gm$date[is.na(gm$date)] <- ref[gm$day[is.na(gm$date)] + 1]
 # Output file
 gm$change <- round(gm$change/100 + 1,2)
 write.table(gm,"timeAtHomeIncreaseCTY.csv",row.names = F,sep=",")
+
+# Updated list of areas to build the look-up
+googleCTY_CNC <- unique(gm$GoogleCTY_CNC)
+write.table(googleCTY_CNC,"googleCTY_CNC_list.csv",row.names = F,sep=",")
