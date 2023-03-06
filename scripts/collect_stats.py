35fb5140d84295296fd0caf84ff876547e7f5dab
Dustin Carlino <dabreegster@gmail.com>
2023-03-06 11:14:06 +0000

    Start a script to use pueue to rerun the pipeline over all inputs, to keep log tracking somewhat sane.
    Adjust how stats collection happens

--- scripts/collect_stats.py
+++ scripts/collect_stats.py
@@ -1,25 +1,19 @@
-# This script runs SPC for all study areas and outputs a table for docs/performance.md
+#!/usr/bin/python3
+# This script takes stats.csv, produced by scripts/run_pipeline_everywhere.sh,
+# and outputs a table for docs/performance.md
 #
 # This has a dependency on `pip install py-markdown-table`
 
 from os import listdir
-import subprocess
 import csv
 from markdownTable import markdownTable
 
 rows = []
-for study_area in sorted(listdir("config")):
-    if study_area == "national.txt":
-        continue
-    # Assume `cargo build --release` has happened
-    subprocess.run(
-        ["./target/release/spc", "config/" + study_area, "--output-stats"], check=True
-    )
-    with open("stats.csv") as f:
-        for row in csv.DictReader(f):
-            rows.append(row)
+with open("stats.csv") as f:
+    for row in csv.DictReader(f):
+        rows.append(row)
 
 # Print as a Markdown table for convenience
-with open("stats.csv") as f:
+with open("perf_table") as f:
     # This still isn't quite Github's format; the +'s are weird
     print(markdownTable(rows).setParams(row_sep="markdown", quote=False).getMarkdown())
