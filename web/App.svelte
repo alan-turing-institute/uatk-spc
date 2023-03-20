e3688147e994a44fbfe640534b51fbbb2478dcd3
mfbenitezp <fbenitez@turing.ac.uk>
2023-03-20 22:59:01 +0000

    updating fonts to make it consistent with SPC font logo

--- web/App.svelte
+++ web/App.svelte
@@ -65,9 +65,19 @@
 {/if}
 
 <style>
+  @font-face {
+          font-family: 'Poppins', sans-serif;
+          font-style: normal;
+          font-weight: 400,600;
+          font-display: swap;
+          src: local('Poppins Regular'), local('Poppins-SemiBold'), url(https://fonts.googleapis.com/css2?family=Poppins:wght@400..600&display=swap) format('woff2');
+          unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+      }
   .base {
     background-color: whitesmoke;
     padding: 10px;
     width: 400px;
+    font-family: 'Poppins', sans-serif;
+
   }
 </style>
