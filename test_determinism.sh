710aebdb9c364d9c2dfd34653e8fc90e01f6039a
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-04-26 21:32:11 +0100

    Refine determinism test script

--- test_determinism.sh
+++ test_determinism.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-cargo run --release -- config/England/rutland.txt --rng-seed 0
-cp data/output/England/2020/rutland.pb test1.pb
-cargo run --release -- config/England/rutland.txt --rng-seed 0
-cp data/output/England/2020/rutland.pb test2.pb
-diff test1.pb test2.pb
+# Use given region
+REGION=$1
 
-python python/protobuf_to_json.py test1.pb > test1.json
-python python/protobuf_to_json.py test2.pb > test2.json
+# Run 1
+cargo run --release -- config/England/$REGION.txt --rng-seed 0
+cp data/output/England/2020/$REGION.pb test1.pb
 
-diff <(jq --sort-keys . test1.json) <(jq --sort-keys . test2.json)
+# Run 2
+cargo run --release -- config/England/$REGION.txt --rng-seed 0
+cp data/output/England/2020/$REGION.pb test2.pb
+
+# Get checksums
+checksum1=`shasum test1.pb | cut -f 1 -d " "`
+checksum2=`shasum test2.pb | cut -f 1 -d " "`
+
+# Compare checksums
+if [ "$checksum1" == "$checksum2" ]; then
+    printf "OK\n"
+else
+    printf "Error: protobuf outputs differ.\n"
+
+    # Convert to JSON
+    python python/protobuf_to_json.py test1.pb > test1.json
+    python python/protobuf_to_json.py test2.pb > test2.json
+
+    # Sort JSON and get diff
+    diff <(jq --sort-keys . test1.json) <(jq --sort-keys . test2.json)
+fi
