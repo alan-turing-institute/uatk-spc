61491d50b066c2dd59790796b2f02cdb4ea14ea3
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-04-26 21:30:44 +0100

    Use prost build config to serialize map with BTreeMap

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 fn main() -> std::io::Result<()> {
-    prost_build::compile_protos(&["src/synthpop.proto"], &["src/"])?;
+    let mut config = prost_build::Config::new();
+    // Config to use BTreeMap over HashMap for deterministic map serialization
+    // See: https://github.com/tokio-rs/prost#using-prost-in-a-no_std-crate
+    config.btree_map(["."]);
+    config.compile_protos(&["src/synthpop.proto"], &["src/"])?;
     Ok(())
 }
