cbe06a4aad8d8338fc6577b5329b9c33c8978a96
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-05-02 11:59:06 +0100

    Refine script for checking determinism

--- scripts/check_determinism.sh
+++ scripts/check_determinism.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Run this script from the repo root with e.g.:
+#
+#   ./scripts/check_determinism.sh rutland
+#
+# passing the region to be checked as the first positional arg.
+#
+# If files differ, the protobuf are converted to sorted JSON to get diff.
+
 # Use given region
 REGION=$1
 
@@ -12,12 +20,18 @@ cargo run --release -- config/England/$REGION.txt --rng-seed 0
 cp data/output/England/2020/$REGION.pb test2.pb
 
 # Get checksums
-checksum1=`shasum test1.pb | cut -f 1 -d " "`
-checksum2=`shasum test2.pb | cut -f 1 -d " "`
+hash1=`shasum -a 256 test1.pb | cut -f 1 -d " "`
+hash2=`shasum -a 256 test2.pb | cut -f 1 -d " "`
+
+printf "SHA256 hash, run 1: ${hash1}\n"
+printf "SHA256 hash, run 2: ${hash2}\n"
 
 # Compare checksums
-if [ "$checksum1" == "$checksum2" ]; then
+if [ $hash1 == $hash2 ]; then
     printf "OK\n"
+
+    # Remove temporary outputs
+    rm test1.pb test2.pb
 else
     printf "Error: protobuf outputs differ.\n"
 
@@ -27,4 +41,7 @@ else
 
     # Sort JSON and get diff
     diff <(jq --sort-keys . test1.json) <(jq --sort-keys . test2.json)
+
+    # Remove temporary outputs
+    rm test1.pb test2.pb test1.json test2.json
 fi
