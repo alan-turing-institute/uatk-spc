91a01e41f00ffff7d4a66b8e973340c85b1c8262
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-06-08 11:22:56 +0100

    Add command line args

--- scripts/data_prep/toAzure_complete.R
+++ scripts/data_prep/toAzure_complete.R
@@ -1,5 +1,12 @@
-folderInOT <- "Data/"
-folderOut <- "Outputs/"
+# Args for file paths
+args <- commandArgs(TRUE)
+print(args)
+
+# File paths
+date <- args[1]
+folderInOT <- args[2]
+spenserInput <- args[3]
+folderOut <- args[4]
 
 ##### Load lookup table (dl for example from https://github.com/alan-turing-institute/uatk-spc/blob/main/scripts/data_prep/SAVE_SPC_required_data.zip)
 lu <- read.csv(paste(folderInOT,"lookUp-GB.csv", sep = ""))
@@ -14,8 +21,8 @@ aref <- unique(lu$AzureRef[lu$Country == Country]) # Destination file
 lads <- unique(lu$LAD20CD[lu$Country == Country]) # Input file
 
 # Special case: England missing lads only
-#aref <- c("dorset","buckinghamshire","leicestershire","suffolk","somerset")
-#lads <- c("E06000058","E06000059","E06000060","E07000135","E07000244","E07000245","E07000246")
+# aref <- c("dorset","buckinghamshire","leicestershire","suffolk","somerset")
+# lads <- c("E06000058","E06000059","E06000060","E07000135","E07000244","E07000245","E07000246")
 
 df <- data.frame(lad = lads, aref = NA)
 for(i in 1:length(lads)){
@@ -31,14 +38,18 @@ popsize = 0 # For control
 for(i in 1:nrow(df2)){
   ref <- unlist(df2$lad[i])
   lad <- ref[1]
-  ladfile <- read.csv(paste("Data/",Country,"/",date,"/",lad,".csv",sep = ""))
+  ladfile <- read.csv(paste(spenserInput,Country,"/",date,"/",lad,".csv",sep = ""))
   if (length(ref) > 1){
     for(j in ref[2:length(ref)]){
-      ladfiletemp <- read.csv(paste("Data/",Country,"/",date,"/",j,".csv",sep = ""))
+      ladfiletemp <- read.csv(paste(spenserInput,Country,"/",date,"/",j,".csv",sep = ""))
       ladfile <- rbind(ladfile,ladfiletemp)
     }
   }
   popsize = popsize + nrow(ladfile)
-  write.table(ladfile,paste(folderOut,"/",Country,"/",date,"/","pop_",df2$aref[i],"_",date,".csv",sep = ""), row.names = F, sep = ",")
+  outpath <- paste(folderOut,"/",Country,"/",date,"/", sep="")
+  outfile <- paste(outpath, "pop_",df2$aref[i],"_",date,".csv",sep = "")
+  print(outfile)
+  dir.create(outpath, recursive=TRUE)
+  write.table(ladfile, outfile, row.names = F, sep = ",")
 }
 popsize
