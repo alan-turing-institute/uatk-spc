a7a1ed6cf72a02e4ac6a11b086a850419180d3a1
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-08-09 10:17:38 +0100

    Add method and fix type in reader, add deps

--- python/uatk_spc/reader.py
+++ python/uatk_spc/reader.py
@@ -13,26 +13,28 @@ class SPCReader:
     info_per_msoa: dict
 
     def __init__(self, path: str):
-        self._read_pop(path)
+        self.pop = SPCReader.read_pop(path)
         pop_as_dict = MessageToDict(self.pop, including_default_value_fields=True)
         self.households = pl.from_records(pop_as_dict["households"])
         self.people = pl.from_records(pop_as_dict["people"])
         self.time_use_diaries = pl.from_records(pop_as_dict["timeUseDiaries"])
         self.venues_per_activity = pop_as_dict["venuesPerActivity"]
-        self.info_per_msoa = pop_as_dict["timeUseDiaries"]
+        self.info_per_msoa = pop_as_dict["infoPerMsoa"]
 
-    def _read_pop(self, file_name: str):
-        self.pop = synthpop_pb2.Population()
+    @classmethod
+    def read_pop(cls, file_name: str) -> synthpop_pb2.Population():
+        pop = synthpop_pb2.Population()
         with open(file_name, "rb") as f:
-            self.pop.ParseFromString(f.read())
+            pop.ParseFromString(f.read())
             f.close()
+        return pop
 
 
 class SPCReaderSplit:
     people: pl.DataFrame
     households: pl.DataFrame
     time_use_diaries: pl.DataFrame
-    venues_per_activity: dict
+    venues_per_activity: pl.DataFrame
     info_per_msoa: dict
 
     def __init__(self, path: str):
